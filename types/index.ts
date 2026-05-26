070dec923a26fe01017b89a95ef35552799e3571
NeavPanjwani56 <Neav.Panjwani.5062582@ves.ac.in>
2026-05-26 14:49:45 +0530

    created ts interfaces for data

---
+++ types/index.ts
@@ -0,0 +1,40 @@
+export interface Course {
+  name: string;
+  degree: string;
+  annualFee: number;
+}
+
+export interface PlacementYear {
+  year: number;
+  avgPackage: number;
+  maxPackage: number;
+  placementRate: number;
+  topRecruiters: string[];
+}
+
+export interface Cutoff {
+  exam: string;
+  year: number;
+  category: string;
+  cutoffValue: number;
+}
+
+export interface College {
+  id: string;
+  slug: string;
+  name: string;
+  city: string;
+  state: string;
+  type: "Government" | "Private" | "Deemed";
+  streams: string[];
+  nirfRank: number;
+  established: number;
+  acreditation: string;
+  website: string;
+  overview: string;
+  annualFee: number;
+  courses: Course[];
+  placements: PlacementYear[];
+  cutoffs: Cutoff[];
+  tags: string[];
+}
