f82e0797d396f94eb9b326c9f1d656fcccf6fe90
NeavPanjwani56 <Neav.Panjwani.5062582@ves.ac.in>
2026-05-29 15:20:00 +0530

    updated next config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "images.weserv.nl" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "logo.clearbit.com" },
+      { protocol: "https", hostname: "ui-avatars.com" },
+    ],
+    unoptimized: true, // Use this for maximum reliability on static exports/CDNs
+  },
 };
 
 export default nextConfig;
